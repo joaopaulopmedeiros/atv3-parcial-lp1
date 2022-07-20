be9fa41b5619bfbd43ea4fa42bf4c34b1d609f4a
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-07-19 21:38:59 -0300

    feat: save map into file

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 class Map
 {
 public:
   Map();
+  int width;
+  int height;
   std::vector<std::vector<int>> cells;
   void saveIntoFile(std::string filePath);
   Map getFromFile(std::string filePath);
@@ -18,6 +22,32 @@ Map::Map()
 
 void Map::saveIntoFile(std::string filePath)
 {
+  std::ofstream file;
+  file.open(filePath, std::ifstream::out);
+  if (file.is_open())
+  {
+    for (int i = 0; i < this->height; i++)
+    {
+      for (int j = 0; j < this->width; j++)
+      {
+        if (j == this->width - 1)
+        {
+          file << this->cells[i][j] << std::endl;
+        }
+        else
+        {
+          file << this->cells[i][j];
+        }
+      }
+    }
+
+    file.close();
+  }
+  else
+  {
+    std::cout << "[ERROR] Arquivo nao encontrado";
+    exit(EXIT_FAILURE);
+  }
 }
 
 Map getFromFile(std::string filePath)
@@ -36,12 +66,22 @@ Map MapSeeder::seed()
 {
   Map m;
 
-  for (size_t i = 0; i < 10; i++)
+  int height = 10;
+  int width = 10;
+
+  m.height = height;
+  m.width = width;
+
+  for (int i = 0; i < height; i++)
   {
-    for (size_t j = 0; j < 10; j++)
+    std::vector<int> cells;
+
+    for (int j = 0; j < width; j++)
     {
-      m.cells[i].push_back(i);
+      cells.push_back(1);
     }
+
+    m.cells.push_back(cells);
   }
 
   return m;
@@ -51,15 +91,19 @@ int main(int argc, char **argv)
 {
   if (argc < 2)
   {
-    std::cout << "Por favor, informe o arquivo com o mapa desejado";
+    std::cout << "[ERROR] Por favor, informe o arquivo com o mapa desejado";
     exit(EXIT_FAILURE);
   }
+
+  std::string filePath = argv[1];
+
+  std::cout << "[INFO] Path do arquivo inputado: " << filePath << std::endl;
 
   MapSeeder seeder;
 
   Map map = seeder.seed();
 
-  map.saveIntoFile(argv[1]);
+  map.saveIntoFile(filePath);
 
   return 0;
 }
